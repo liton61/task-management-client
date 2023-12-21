d33086ed929c73f83ca838eed5d8391d31cde5a1
liton61 <litonofficial61@gmail.com>
2023-12-21 10:37:51 +0600

    banner added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,16 @@
 const Banner = () => {
     return (
         <div>
-            <h1>Banner</h1>
+            <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.postimg.cc/85hnjWtD/bg.webp)' }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">Task Management</h1>
+                        <p className="mb-5">A Task Management System is a software tool or platform designed to help individuals or teams organize, track, prioritize, and complete tasks efficiently.</p>
+                        <button className="btn btn-primary">Let’s Explore <i className="fa-solid fa-arrow-right"></i></button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
